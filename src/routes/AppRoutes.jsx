8267ebfdd751ad4763bca409688a39f82e85fbae
Oatake <thitiwut.ekwisit@gmail.com>
2025-01-07 20:08:11 +0700

    Initial : Project Structure Template

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,8 @@
+function AppRoutes() {
+  return (
+    <h1 className="text-3xl font-bold underline">   
+        AppRoute!!
+    </h1>
+  )
+}
+export default AppRoutes
---
+++ src/utils/config/axios-config.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL="http://localhost:8890/"
+
+export default axios
