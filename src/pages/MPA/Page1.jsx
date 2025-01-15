c5953259309c19d8d731d7041bb2cf77a0476b5e
SupakornPt <supakornpattayanant@gmail.com>
2025-01-15 22:47:02 +0700

    start add : multi-page-signup

---
+++ src/pages/MPA/Page1.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+export default function App() {
+
+    const logo = "https://picsum.photos/500/500";
+    const contextMain = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, ";
+    const contextSup = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, ";
+    const buttonName = "GET STARTED"
+    const listOptionLogin = [1, 2, 3, 4];
+
+    return (
+        <div className='min-h-screen bg-red-500 flex flex-col justify-center items-center'>
+            <div className='flex items-center flex-col bg-green-500 gap-10'>
+                <div className='flex overflow-hidden w-[200px] h-[200px]'>
+                    <img src={logo} alt="" className='w-full' />
+                </div>
+                <div className='flex flex-col items-center'>
+                    <div className='text-[2vw] font-bold'>{contextMain}</div>
+                    <div className='text-[1.5vw] font-semibold'>{contextSup}</div>
+                </div>
+                <div className='text-[1vw] font-bold flex justify-center'>
+                    <button className='bg-slate-300 py-3 px-5'>{buttonName}</button>
+                </div>
+                <div className='text-[1vw] font-bold'>or</div>
+                <div className='flex flex-row gap-10 '>
+                    {listOptionLogin.map((option) => {
+                        return <button>{option}</button>
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/MPA/Page2.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+export default function App() {
+
+    const logo = "https://picsum.photos/500/500";
+    const nameOfLabel = "Username / E-mail / Phone"
+    const buttonName = "Continue"
+
+    return (
+        <div className='min-h-screen bg-red-500 flex flex-col justify-center items-center'>
+            <div className='bg-green-500 flex flex-col items-center gap-10'>
+                <div className='flex overflow-hidden w-[200px] h-[200px]'>
+                    <img src={logo} alt="" className='w-full' />
+                </div>
+                <div>
+                    <div className='ml-[10px] text-[1vw] font-semibold'>
+                        <label htmlFor="">{nameOfLabel}</label>
+                    </div>
+                    <div className='text-[1.25vw]'>
+                        <input type="text" className='p-2 w-[500px] rounded-lg' />
+                    </div>
+                </div>
+                <div className='text-[1vw] font-bold flex justify-center'>
+                    <button className='bg-slate-300 py-3 px-5'>{buttonName}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
