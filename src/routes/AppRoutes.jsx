fa5b26ac442c8b996a538101b64b9c8740df96a4
Oatake <thitiwut.ekwisit@gmail.com>
2025-01-16 01:16:23 +0700

    reviseErrorAtAppRouteConfig

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,10 @@
-import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routerConfig } from "./config/AppRouters-config";
+
 const router = createBrowserRouter(routerConfig);
 
 function AppRoutes() {
   return (
-    //  <h1 className="text-3xl font-bold underline">
-    //       AppRoute!!
-    //   </h1>
-
     <div>
       <RouterProvider router={router} />
     </div>
