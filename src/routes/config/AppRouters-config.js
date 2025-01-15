e92dc75daada00ca8187082457cdf988bcd2acb7
Oatake <thitiwut.ekwisit@gmail.com>
2025-01-15 19:54:19 +0700

    initial-approutes

---
+++ src/routes/config/AppRouters-config.js
@@ -0,0 +1,15 @@
+const routerConfig = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // { path: "/", element: <Home /> },
+      { index: true, element: <Home /> },
+      { path: "SPA", element: <Home /> },
+      { path: "MPA", element: <Home /> },
+
+    ],
+  },
+]
+
+export { routerConfig };
